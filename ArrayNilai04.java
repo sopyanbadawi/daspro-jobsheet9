b427fe51ebcda852aae6d362932a895427f6c9e1
sopyanbadawi <sopyanbadawi.com>
2024-10-30 09:27:07 +0700

    Array Nilai 1

---
+++ ArrayNilai04.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ArrayNilai04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] nilaiAkhir = new int[10];
+
+        for (int i = 0; i < 10; i++){
+            System.out.print("Masukkan nikai akhir ke-" + i + " : ");
+            nilaiAkhir[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i<10; i++) {
+            System.out.println("Nilai akhir ke-" +i+ " adalah " + nilaiAkhir[i]);
+        }
+    }
+}
