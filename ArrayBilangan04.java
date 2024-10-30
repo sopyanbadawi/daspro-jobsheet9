c56a5e92a5705dd25dbe02caa34e542e8a6a3f13
sopyanbadawi <sopyanbadawi.com>
2024-10-30 08:45:12 +0700

    Array Bilangan 2

--- ArrayBilangan04.java
+++ ArrayBilangan04.java
@@ -1,16 +1,12 @@
 public class ArrayBilangan04 {
     public static void main(String[] args) {
       
-        int[] bil = new int[4];
-        bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
+        double[] bil = {5.0, 12867, 7.5, 2000000};
+    
         
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        for(int i = 0; i <= 4; i++){
+            System.out.println(bil[i]);
+        }
     }
     
 }
